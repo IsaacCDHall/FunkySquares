8a771d996e5d77047cec9d0eae999911295c2291
Isaac Hall <isaac.hall@nike.com>
2021-03-30 21:34:39 -0700

    create interactable mondrian art

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+ const colors = [
+    "red",
+    "white",
+    "blue",
+    "yellow"
+  ];
+ const dividers = [
+    "horizontalDivider",
+    "verticalDivider"
+  ];
+  const getRandomInt = (max)=> {
+    return parseInt(Math.floor(Math.random() * Math.floor(max)));
+  }
+  const squares = [];
+  const generateSquares= ()=>{
+    for(let i = 0; i<60; i++){
+      squares.push(
+        <div onClick={(e)=>{
+          // randomizes color when clicking instead from 4 choices
+          e.target.className = `funkySquare ${colors[getRandomInt(4)]}`;
+        }} className={`funkySquare ${colors[getRandomInt(4)]}`} key={i}>
+          {/* randomizes original layout on render aka every refresh looks different */}
+          <div className={`${dividers[getRandomInt(2)]}`}></div>
+        </div>
+        
+      )
+    }
+  }
+  generateSquares();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <div className="app">
+      <main className="container">
+        {squares}          
+      </main>
+  </div>
+  )}
 export default App;
